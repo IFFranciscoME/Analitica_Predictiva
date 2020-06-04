14ec6fbd55ca6bff783d7bcfb50233955bf05bdb
Francisco ME <if.francisco.me@gmail.com>
2020-06-04 16:24:46 -0500

    base

---
+++ ap_tarea_1.R
@@ -0,0 +1,9 @@
+
+# -- ------------------------------------------------------------------------------------- -- #
+# -- Project title: Analitica Predictiva                                                   -- #
+# -- Project description: Notas del curso analitica predictiva                             -- #
+# -- file: ap_tarea_1.R - Script con ejercicio de tarea 1                                  -- #
+# -- Repository: https://github.com/IFFranciscoME/Analitica_Predictiva                     -- #
+# -- Author: Francisco ME                                                                  -- #
+# -- ------------------------------------------------------------------------------------- -- #
+
